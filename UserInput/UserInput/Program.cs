7c47f2f766c69e1ce8f9bdb8229d5584d4eb6f3f
Aman Maharjan <neutrino2039@gmail.com>
2020-09-02 11:58:31 +0545

    User input program

---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("mass: ");
+            double m = double.Parse(Console.ReadLine());
+
+            Console.Write("acceleration: ");
+            double a = double.Parse(Console.ReadLine());
+
+            double f = m * a;
+            Console.WriteLine($"Force = {f}");
+        }
+    }
+}
