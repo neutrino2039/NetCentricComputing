a0c619dac432bc22d75df52dd6966b7a67534bb2
Aman Maharjan <neutrino2039@gmail.com>
2020-09-06 12:28:48 +0545

    Switch statement demo program

---
+++ SwitchDemo/SwitchDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SwitchDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Principal: ");
+            double p = double.Parse(Console.ReadLine());
+
+            Console.Write("Time: ");
+            double t = double.Parse(Console.ReadLine());
+
+            Console.Write("Account Type ");
+            Console.Write("(Saving = S, Fixed = F): ");
+            string type = Console.ReadLine().ToUpper();
+
+            double r = type switch
+            {
+                "S" => 2.5,
+                "F" => 7.0,
+                _ => 0.5,
+            };
+
+            double i = p * t * r / 100;
+            Console.WriteLine($"Interest = {i}");
+        }
+    }
+}
