46d1303e96311d5f0c16a2f04511bd9087109a60
Aman Maharjan <neutrino2039@gmail.com>
2020-09-08 14:00:23 +0545

    Sum of array (for loop) program

---
+++ SumOfArray/SumOfArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SumOfArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] data = { 5, 10, 15, 20, 25 };
+
+            int sum = 0;
+            for (int i = 0; i < data.Length; i++)
+                sum += data[i];
+
+            Console.WriteLine($"Sum = {sum}");
+        }
+    }
+}
