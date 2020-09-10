eac7395a9df23c5b5a945bab7420150e48a22c62
Aman Maharjan <neutrino2039@gmail.com>
2020-09-10 12:37:24 +0545

    Sum of array (with for each) program

---
+++ SumOfArrayWithForEach/SumOfArrayWithForEach/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SumOfArrayWithForEach
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] data = { 5, 10, 15, 20, 25 };
+
+            int sum = 0;
+            foreach (int element in data)
+                sum += element;
+
+            Console.WriteLine($"Sum = {sum}");
+        }
+    }
+}
