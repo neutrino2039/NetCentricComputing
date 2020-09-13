701946d8327b2c75a1c3a3a94d3ec28420761c23
Aman Maharjan <neutrino2039@gmail.com>
2020-09-13 12:30:05 +0545

    User input using function (v1)

---
+++ UserInputUsingFunction/UserInputUsingFunction/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UserInputUsingFunction
+{
+    class Program
+    {
+        static void Main()
+        {
+            double m = InputDouble("mass: ");
+            double a = InputDouble("acceleration: ");
+
+            double f = m * a;
+            Console.WriteLine($"Force = {f}");
+        }
+
+        private static double InputDouble(string displayText)
+        {
+            Console.Write(displayText);
+            return double.Parse(Console.ReadLine());
+        }
+    }
+}
