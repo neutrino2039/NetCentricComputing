301d2a8accbfbc5e89d12eb765831061bfbc6b85
Aman Maharjan <neutrino2039@gmail.com>
2020-09-13 12:30:19 +0545

    Guessing game using functions (v1)

---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main()
+        {
+            string choice;
+            do
+            {
+                Random random = new Random();
+                int randomNumber = random.Next(1, 101);
+                Console.WriteLine($"generated number = {randomNumber}");
+
+                bool userWon = false;
+
+                for (int i = 1; i <= 5; i++)
+                {
+                    Console.Write("guess: ");
+                    int guess = int.Parse(Console.ReadLine());
+                    if (guess == randomNumber)
+                    {
+                        Console.WriteLine("Hooray! You won.");
+                        userWon = true;
+                        break;
+                    }
+                    else if (guess > randomNumber)
+                        Console.WriteLine("Your guess is greater than my number.");
+                    else
+                        Console.WriteLine("Your guess is less than my number.");
+                }
+
+                if (!userWon)
+                {
+                    Console.WriteLine("Oh no! You lost.");
+                    Console.WriteLine($"Correct number is: {randomNumber}");
+                }
+
+                Console.Write("Replay (Y/N)? ");
+                choice = Console.ReadLine().ToLower();
+            } while (choice == "y");
+        }
+    }
+}
---
+++ GuessingGameUsingFunctions/GuessingGameUsingFunctions/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace GuessingGameUsingFunctions
+{
+    class Program
+    {
+        static void Main()
+        {
+            do
+                StartNewGame();
+            while (ConfirmReplay() == "Y");
+        }
+
+        private static string ConfirmReplay()
+        {
+            Console.Write("Replay (Y/N)? ");
+            return Console.ReadLine().ToUpper();
+        }
+
+        private static void StartNewGame()
+        {
+            int randomNumber = GenerateRandomNumber();
+
+            for (int i = 0; i <= 5; i++)
+            {
+                int guess = InputInt("guess: ");
+                if (guess == randomNumber)
+                {
+                    Console.WriteLine("Hooray! You won.");
+                    return;
+                }
+                else if (guess < randomNumber)
+                    Console.WriteLine("Your guess is less than my number.");
+                else
+                    Console.WriteLine("Your guess is greater than my number.");
+            }
+
+            Console.WriteLine("Oh no! You lost.");
+            Console.WriteLine($"The correct number is {randomNumber}");
+        }
+
+        private static int InputInt(string displayText)
+        {
+            Console.Write(displayText);
+            return int.Parse(Console.ReadLine());
+        }
+
+        private static int GenerateRandomNumber()
+        {
+            Random random = new Random();
+            return random.Next(1, 101);
+        }
+    }
+}
