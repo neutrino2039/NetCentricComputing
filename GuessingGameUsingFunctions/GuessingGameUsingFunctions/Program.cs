ce603c2cfd663a30cf5a68522c23782318708599
Aman Maharjan <neutrino2039@gmail.com>
2020-10-05 12:17:11 +0545

    Guessing game using functions (v2)

--- GuessingGameUsingFunctions/GuessingGameUsingFunctions/Program.cs
+++ GuessingGameUsingFunctions/GuessingGameUsingFunctions/Program.cs
@@ -8,38 +8,55 @@ namespace GuessingGameUsingFunctions
         {
             do
                 StartNewGame();
-            while (ConfirmReplay() == "Y");
+            while (UserWantsToReplay());
         }
 
-        private static string ConfirmReplay()
+        private static bool UserWantsToReplay()
         {
             Console.Write("Replay (Y/N)? ");
-            return Console.ReadLine().ToUpper();
+            return Console.ReadLine().ToUpper() == "Y";
         }
 
         private static void StartNewGame()
         {
             int randomNumber = GenerateRandomNumber();
+            AskUserToGuess(randomNumber);
+        }
 
+        private static void AskUserToGuess(int randomNumber)
+        {
             for (int i = 0; i <= 5; i++)
             {
-                int guess = InputInt("guess: ");
-                if (guess == randomNumber)
-                {
-                    Console.WriteLine("Hooray! You won.");
-                    return;
-                }
-                else if (guess < randomNumber)
-                    Console.WriteLine("Your guess is less than my number.");
-                else
-                    Console.WriteLine("Your guess is greater than my number.");
+                int guess = Input("guess: ");
+                bool correctGuess = CheckGuess(guess, randomNumber);
+                if (correctGuess) return;
             }
 
+            ShowLostMessage(randomNumber);
+        }
+
+        private static bool CheckGuess(int guess, int randomNumber)
+        {
+            if (guess == randomNumber)
+            {
+                Console.WriteLine("Hooray! You won.");
+                return true;
+            }
+            else if (guess < randomNumber)
+                Console.WriteLine("Your guess is less than my number.");
+            else
+                Console.WriteLine("Your guess is greater than my number.");
+
+            return false;
+        }
+
+        private static void ShowLostMessage(int randomNumber)
+        {
             Console.WriteLine("Oh no! You lost.");
             Console.WriteLine($"The correct number is {randomNumber}");
         }
 
-        private static int InputInt(string displayText)
+        private static int Input(string displayText)
         {
             Console.Write(displayText);
             return int.Parse(Console.ReadLine());
