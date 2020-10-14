ecf79f4a1bd3ffdb232d60ba0fcef128f0836887
Aman Maharjan <neutrino2039@gmail.com>
2020-10-14 12:27:53 +0545

    Desktop arithmetic (v2)

---
+++ DesktopArithmeticV2/DesktopArithmetic/MainForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace DesktopArithmetic
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.aTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sumTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "a:";
+            // 
+            // aTextBox
+            // 
+            this.aTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.aTextBox.Location = new System.Drawing.Point(45, 13);
+            this.aTextBox.Name = "aTextBox";
+            this.aTextBox.Size = new System.Drawing.Size(214, 20);
+            this.aTextBox.TabIndex = 1;
+            this.aTextBox.TextChanged += new System.EventHandler(this.ATextBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "b:";
+            // 
+            // bTextBox
+            // 
+            this.bTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bTextBox.Location = new System.Drawing.Point(45, 39);
+            this.bTextBox.Name = "bTextBox";
+            this.bTextBox.Size = new System.Drawing.Size(214, 20);
+            this.bTextBox.TabIndex = 3;
+            this.bTextBox.TextChanged += new System.EventHandler(this.BTextBox_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "sum:";
+            // 
+            // sumTextBox
+            // 
+            this.sumTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sumTextBox.Location = new System.Drawing.Point(45, 65);
+            this.sumTextBox.Name = "sumTextBox";
+            this.sumTextBox.ReadOnly = true;
+            this.sumTextBox.Size = new System.Drawing.Size(214, 20);
+            this.sumTextBox.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 101);
+            this.Controls.Add(this.sumTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.bTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.aTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Arithmetic";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ErrorProvider errorProvider;
+        private System.Windows.Forms.TextBox sumTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox bTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox aTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DesktopArithmeticV2/DesktopArithmetic/MainForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopArithmetic
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateSum()
+        {
+            double a = 0, b = 0;
+
+            errorProvider.Clear();
+
+            try
+            {
+                a = double.Parse(aTextBox.Text);
+            }
+            catch
+            {
+                errorProvider.SetError(aTextBox, "Please enter a valid number");
+            }
+
+            try
+            {
+                b = double.Parse(bTextBox.Text);
+            }
+            catch
+            {
+                errorProvider.SetError(bTextBox, "Please enter a valid number");
+            }
+
+            double sum = a + b;
+            sumTextBox.Text = sum.ToString();
+        }
+
+        private void ATextBox_TextChanged(object sender, EventArgs e)
+        {
+            CalculateSum();
+        }
+
+        private void BTextBox_TextChanged(object sender, EventArgs e)
+        {
+            CalculateSum();
+        }
+    }
+}
