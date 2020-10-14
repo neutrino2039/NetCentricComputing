8c5db3947be65db27064174d54658f5ea5861ef1
Aman Maharjan <neutrino2039@gmail.com>
2020-10-14 12:28:37 +0545

    Digital clock with custom font

---
+++ DigitalClock/DigitalClock/MainForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DigitalClock
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.timeLabel.Font = new System.Drawing.Font("Monotype Corsiva", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(12, 9);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(287, 119);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "?";
+            this.timeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 137);
+            this.Controls.Add(this.timeLabel);
+            this.Name = "MainForm";
+            this.Text = "Digital Clock";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ DigitalClock/DigitalClock/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Windows.Forms;
+
+namespace DigitalClock
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            SetCustomFont();
+            UpdateTime();
+        }
+
+        private void SetCustomFont()
+        {
+            PrivateFontCollection pfc = new PrivateFontCollection();
+            pfc.AddFontFile("Fonts/Digital7Mono.ttf");
+            timeLabel.Font = new Font(pfc.Families[0], timeLabel.Font.Size);
+        }
+
+        private void UpdateTime()
+        {
+            DateTime now = DateTime.Now;
+            timeLabel.Text = $"{now.Hour:D2}:{now.Minute:D2}:{now.Second:D2}";
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateTime();
+        }
+    }
+}
