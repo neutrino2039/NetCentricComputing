c74f2618b6d2021c1c274913947982d70105166c
Aman Maharjan <neutrino2039@gmail.com>
2020-11-18 12:24:15 +0545

    Inheritance using custom control

---
+++ OOP/CustomControl/CustomControl/MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace CustomControl
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rectangle = new CustomControl.Rectangle();
+            this.SuspendLayout();
+            // 
+            // rectangle
+            // 
+            this.rectangle.Location = new System.Drawing.Point(74, 52);
+            this.rectangle.Name = "rectangle";
+            this.rectangle.Size = new System.Drawing.Size(142, 137);
+            this.rectangle.StrokeColor = System.Drawing.Color.DarkRed;
+            this.rectangle.StrokeWidth = 10F;
+            this.rectangle.TabIndex = 0;
+            this.rectangle.Text = "rectangle1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 282);
+            this.Controls.Add(this.rectangle);
+            this.Name = "MainForm";
+            this.Text = "Custom Control Demo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Rectangle rectangle;
+    }
+}
+
---
+++ OOP/CustomControl/CustomControl/Rectangle.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace CustomControl
+{
+    public class Rectangle : Control
+    {
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics g = e.Graphics;
+
+            Pen pen = new Pen(StrokeColor, StrokeWidth);
+            g.DrawRectangle(pen, 0, 0, Width - 1, Height - 1);
+        }
+
+        private Color strokeColor = Color.Black;
+        public Color StrokeColor
+        {
+            get { return strokeColor; }
+            set
+            {
+                strokeColor = value;
+                Invalidate();
+            }
+        }
+
+        private float strokeWidth = 1;
+        public float StrokeWidth
+        {
+            get { return strokeWidth; }
+            set
+            {
+                strokeWidth = value;
+                Invalidate();
+            }
+        }
+    }
+}
