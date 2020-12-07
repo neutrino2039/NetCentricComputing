40794a71bfeadb3d402de0733df819410fda9dba
Aman Maharjan <neutrino2039@gmail.com>
2020-12-07 19:32:17 +0545

    Stack demo

---
+++ Generics/StackDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+
+var stack = new Stack<char>();
+
+string text = "hello world";
+
+foreach(var character in text)
+    stack.Push(character);
+
+while(stack.Count > 0)
+    Write(stack.Pop());
+
+WriteLine();
+
