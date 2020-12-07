a93bda8aea712448a4ac08c8c68bbbfe87b18c22
Aman Maharjan <neutrino2039@gmail.com>
2020-12-07 19:32:26 +0545

    Dictionary demo

---
+++ Generics/DictionaryDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+
+var openWith = new Dictionary<string, string>();
+
+openWith.Add("txt", "notepad.exe");
+openWith.Add("bmp", "paint.exe");
+openWith["rtf"] = "wordpad.exe"; // indexer syntax
+
+if(openWith.ContainsKey("txt"))
+    WriteLine(openWith["txt"]); // indexer syntax
+
+// foreach(var key in openWith.Keys)
+//     WriteLine($"{key} opens with {openWith[key]}");
