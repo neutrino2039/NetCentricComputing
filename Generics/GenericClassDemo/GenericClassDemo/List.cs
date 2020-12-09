cd403dbfe616f540d906a431c7439225013d511a
Aman Maharjan <neutrino2039@gmail.com>
2020-12-09 22:26:38 +0545

    Generic class

---
+++ Generics/GenericClassDemo/GenericClassDemo/List.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class List<T>
+{
+    private const int DEFAULT_CAPACITY = 2;
+    private T[] data = new T[DEFAULT_CAPACITY];
+
+    public int Capacity { get; set; } = DEFAULT_CAPACITY;
+
+    public int Count { get; private set; } = 0;
+
+    public void Add(T item)
+    {
+        if(Capacity == Count)
+        {
+            Capacity *= 2;
+            var temp = new T[Capacity];
+            Array.Copy(data, temp, data.Length);
+            data = temp;
+        }
+        data[Count++] = item;
+    }
+
+    public T this[int index]
+    {
+        get { return data[index]; }
+        set { data[index] = value; }
+    }
+}
---
+++ Generics/GenericClassDemo/GenericClassDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+var numbers = new List<int>();
+numbers.Add(5);
+numbers.Add(10);
+numbers[1] = 20;
+numbers.Add(50);
+
+//WriteLine(numbers[1]);
+
+for (int i = 0; i < numbers.Count; i++)
+    WriteLine(numbers[i]);
+
+var words = new List<string>();
+words.Add("Hello");
+words.Add("World");
+words[0] = "Hello Again";
+
+//WriteLine(words[0]);
+
+for (int i = 0; i < words.Count; i++)
+    WriteLine(words[i]);
