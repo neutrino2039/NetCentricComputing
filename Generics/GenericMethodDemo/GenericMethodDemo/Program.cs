71aa9122171142e7712ef84cda7fa6ba7fedc045
Aman Maharjan <neutrino2039@gmail.com>
2020-12-09 22:25:32 +0545

    Generic method

---
+++ Generics/GenericMethodDemo/GenericMethodDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+static void Swap<Type>(ref Type a, ref Type b)
+{
+    Type temp = a;
+    a = b;
+    b = temp;
+}
+
+int a = 5, b = 10;
+Swap(ref a, ref b);
+WriteLine($"a = {a}, b = {b}");
+
+string c = "Hello", d = "World";
+Swap(ref c, ref d);
+WriteLine($"c = {c}, d = {d}");
