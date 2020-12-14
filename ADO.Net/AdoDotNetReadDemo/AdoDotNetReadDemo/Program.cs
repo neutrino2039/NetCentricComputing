cdf5a9dc6049248eaa85363c8229cfcdff8316c3
Aman Maharjan <neutrino2039@gmail.com>
2020-12-14 12:14:34 +0545

    ADO.Net read data demo

---
+++ ADO.Net/AdoDotNetReadDemo/AdoDotNetReadDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.SqlClient;
+using static System.Console;
+
+namespace AdoDotNetReadDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            var connectionString =
+                @"Data Source=.;"
+                + "Initial Catalog=Trinity;"
+                + "Integrated Security=True;";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var sql = "select * from Students";
+                var command = new SqlCommand(sql, connection);
+
+                connection.Open();
+                var reader = command.ExecuteReader();
+                while(reader.Read())
+                {
+                    WriteLine($"Id = {reader["Id"]}");
+                    WriteLine($"First Name = {reader["FirstName"]}");
+                    WriteLine($"Last Name = {reader["LastName"]}");
+                    WriteLine();
+                }
+            }
+        }
+    }
+}
