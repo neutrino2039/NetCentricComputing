9eb813de9dc9c26369d0383b93ce1421540bedff
Aman Maharjan <neutrino2039@gmail.com>
2020-12-14 12:14:16 +0545

    Delegate demo

---
+++ OOP/DelegateDemo/DelegateDemo/Helper.cs
@@ -0,0 +1,20 @@
+﻿public class Helper
+{
+    public delegate void Callback(ref int item);
+
+    public static void ForEach(int[] array, Callback callback)
+    {
+        for (int i = 0; i < array.Length; i++)
+            callback(ref array[i]);
+    }
+
+    public static void Double(ref int item)
+    {
+        item *= 2;
+    }
+
+    public static void Half(ref int item)
+    {
+        item /= 2;
+    }
+}
---
+++ OOP/DelegateDemo/DelegateDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+using static Helper;
+
+static void PrintArray(int[] array)
+{
+    WriteLine(string.Join(", ", array));
+}
+
+int[] data = { 1, 2, 3, 4, 5 };
+PrintArray(data);
+
+ForEach(data, Double);
+PrintArray(data);
+
+ForEach(data, Half);
+PrintArray(data);
+
+// anonymous delegate
+ForEach(data, delegate (ref int item) {
+    item *= 10;
+});
+PrintArray(data);
+
+// lamda expression
+ForEach(data, (ref int item) => {
+    item /= 10;
+});
+PrintArray(data);
