3c099b24e5bf105e203b4be203f48097ebe7418e
Aman Maharjan <neutrino2039@gmail.com>
2020-12-16 12:15:03 +0545

    ADO.Net read scalar data

---
+++ ADO.Net/AdoDotNetReadScalarData/AdoDotNetReadScalarData/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+using System.Data.SqlClient;
+
+namespace AdoDotNetReadScalarData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString =
+                @"Data Source=(localdb)\ProjectsV13;"
+                + "Initial Catalog=Trinity;"
+                + "Integrated Security=True;";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var sql = "select count(*) from Students";
+                var command = new SqlCommand(sql, connection);
+
+                connection.Open();
+                var studentCount = command.ExecuteScalar();
+                WriteLine($"Student Count = {studentCount}");
+            }
+        }
+    }
+}
