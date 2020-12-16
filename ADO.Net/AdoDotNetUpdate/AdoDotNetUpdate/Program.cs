5fbe4757b93504d00e4c90f283183f5ae3244ea9
Aman Maharjan <neutrino2039@gmail.com>
2020-12-16 12:16:19 +0545

    ADO.Net update data

---
+++ ADO.Net/AdoDotNetUpdate/AdoDotNetUpdate/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+using System.Data.SqlClient;
+
+namespace AdoDotNetUpdate
+{
+    class Program
+    {
+        static void Main()
+        {
+            var connectionString =
+                @"Data Source=(localdb)\ProjectsV13;"
+                + "Initial Catalog=Trinity;"
+                + "Integrated Security=True;";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var sql = "update Teachers set FirstName = 'Test (edited)', LastName = 'Test (edited 2)' where Id = 3";
+                var command = new SqlCommand(sql, connection);
+
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                    WriteLine("Record updated successfully.");
+                else
+                    WriteLine("Unable to update record.");
+            }
+        }
+    }
+}
