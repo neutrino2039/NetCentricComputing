2da008716571bd7259e06450b47aec19ab54fca9
Aman Maharjan <neutrino2039@gmail.com>
2020-12-16 12:14:38 +0545

    ADO.Net read data with DataAdapter

---
+++ ADO.Net/AdoDotNetDataAdapterReadDemo/AdoDotNetDataAdapterReadDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace AdoDotNetDataAdapterReadDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            var connectionString =
+                @"Data Source=(localdb)\ProjectsV13;"
+                + "Initial Catalog=Trinity;"
+                + "Integrated Security=True;";
+            var sql = "select * from Students";
+            using (var adapter = new SqlDataAdapter(sql, connectionString))
+            {
+                var table = new DataTable();
+                adapter.Fill(table);
+                foreach(DataRow row in table.Rows)
+                {
+                    WriteLine($"Id = {row["Id"]}");
+                    WriteLine($"First Name = {row["FirstName"]}");
+                    WriteLine($"Last Name = {row["LastName"]}");
+                    WriteLine();
+                }
+            }
+        }
+    }
+}
