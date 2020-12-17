9adb0da88625c236734508ee715e3664c554f3b7
Aman Maharjan <neutrino2039@gmail.com>
2020-12-17 10:43:01 +0545

    ADO.Net read data demo with MySql

---
+++ ADO.Net/AdoDotNetMySqlReadDemo/AdoDotNetMySqlReadDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using static System.Console;
+
+namespace AdoDotNetMySqlReadDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            var connectionString =
+                "Server = localhost;"
+                + "Database = trinity;"
+                + "Uid = root;"
+                + "Pwd = ";
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                var sql = "select * from students";
+                var command = new MySqlCommand(sql, connection);
+
+                connection.Open();
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    WriteLine($"Id = {reader["id"]}");
+                    WriteLine($"First Name = {reader["first_name"]}");
+                    WriteLine($"Last Name = {reader["last_name"]}");
+                    WriteLine();
+                }
+            }
+        }
+    }
+}
