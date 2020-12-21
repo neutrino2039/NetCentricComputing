05c08c429fa1b089a7732e7d272cc003280b318d
Aman Maharjan <neutrino2039@gmail.com>
2020-12-21 12:53:45 +0545

    Extension method demo

---
+++ Basics/ExtensionMethodDemo/ExtensionMethodDemo/Program.cs
@@ -0,0 +1,6 @@
+﻿using static System.Console;
+using ExtensionMethodDemo;
+
+var text = "helLO extension meTHOD";
+WriteLine(text);
+WriteLine(text.ToProper());
---
+++ Basics/ExtensionMethodDemo/ExtensionMethodDemo/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace ExtensionMethodDemo
+{
+    public static class StringExtensions
+    {
+        public static string ToProper(this string text)
+        {
+            if (string.IsNullOrWhiteSpace(text)) return text;
+
+            var result = new StringBuilder();
+            result.Append(char.ToUpper(text[0]));
+
+            for (int i = 1; i < text.Length; i++)
+            {
+                var previousCharacter = text[i - 1];
+                var currentCharacter = text[i];
+                if (char.IsWhiteSpace(previousCharacter))
+                    currentCharacter = char.ToUpper(currentCharacter);
+                else
+                    currentCharacter = char.ToLower(currentCharacter);
+                result.Append(currentCharacter);
+            }
+
+            return result.ToString();
+        }
+    }
+}
